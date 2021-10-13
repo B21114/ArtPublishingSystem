e2e50d534841af96b8ee91133e45af313c2b6504
B21114 <b21114@yandex.ru>
2021-10-13 16:11:24 +0400

    #6 Добавить обработчик для загрузки контента

---
+++ ContentManagementSubsystem/APS.CMS.Application/Publications/Commands/CreatePublication/CreatePublicationRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace APS.CMS.Application.Publications.Commands.CreatePublication
+{
+    /// <summary>
+    /// Обработчик запросов.
+    /// </summary>
+    public class CreatePublicationRequestHandler : IRequestHandler<CreatePublicationRequest, CreatePublicationResponse>
+    {
+        public Task<CreatePublicationResponse> Handle(CreatePublicationRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+}
