3e009b05f8851e21a5fe9f79d3f01d7cb122ebcb
District_11 <baira.98@mail.ru>
2021-10-15 13:50:56 +0400

    Добавление конструктора для GetPublicationByIdRequestHandler и добавление комментариев

--- ContentManagementSubsystem/APS.CMS.Application/Publications/Queries/GetPublicationById/GetPublicationByIdRequestHandler.cs
+++ ContentManagementSubsystem/APS.CMS.Application/Publications/Queries/GetPublicationById/GetPublicationByIdRequestHandler.cs
@@ -20,6 +20,26 @@ namespace APS.CMS.Application.Publications.Queries.GetPublicationById
         private readonly IHttpContextAccessor _httpContextAccessor;
         private IMapper _mapper;
 
+        /// <summary>
+        /// Конструтктор обработчиков запроса публикаций
+        /// </summary>
+        /// <param name="contentDbContext">Контекст базы данных предоставляющий контент.</param>
+        /// <param name="httpContextAccessor">Предоставляет доступ к текущему пользователю, если он доступен.</param>
+        /// <param name="mapper">Маппер.</param>
+        public GetPublicationByIdRequestHandler(IContentDbContext contentDbContext, IHttpContextAccessor httpContextAccessor, IMapper mapper)
+        {
+            _contentDbContext = contentDbContext;
+            _httpContextAccessor = httpContextAccessor;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Поток обрабатывает запрос, отвечает значением типа GetPublicationByIdResponse,
+        /// запрашивает тип GetPublicationByIdRequest.
+        /// </summary>
+        /// <param name="request">Запрос, публичный ли файл.</param>
+        /// <param name="cancellationToken">Объект для наблюдения за ожиданием завершения задачи.</param>
+        /// <returns></returns>
         public async Task<GetPublicationByIdResponse> Handle(GetPublicationByIdRequest request, CancellationToken cancellationToken)
         {
             var contentEntity = await _contentDbContext.Contents.FindAsync(request.Id);
