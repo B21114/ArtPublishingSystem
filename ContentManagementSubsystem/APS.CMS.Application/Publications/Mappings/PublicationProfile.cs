63e35cd87ff8b97760c8536734daf36f9bb91c4f
District_11 <baira.98@mail.ru>
2021-10-15 11:41:57 +0400

    Добавил обработчик для получения контента по Id

---
+++ ContentManagementSubsystem/APS.CMS.Application/Publications/Mappings/PublicationProfile.cs
@@ -0,0 +1,20 @@
+﻿using APS.CMS.Application.Publications.Queries.GetPublicationById;
+using APS.DBS.Domain.Entities;
+using AutoMapper;
+
+namespace APS.CMS.Application.Publications.Mappings
+{
+    /// <summary>
+    /// Класс публичного экзепляра.
+    /// </summary>
+    public class PublicationProfile : Profile
+    {
+        /// <summary>
+        /// Конструктор публичного экземпляра.
+        /// </summary>
+        public PublicationProfile()
+        {
+            CreateMap<Content, PublicationDetailsDto>();
+        }
+    }
+}
---
+++ ContentManagementSubsystem/APS.CMS.Application/Publications/Queries/GetPublicationById/PublicationDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APS.CMS.Application.Publications.Queries.GetPublicationById
+{
+    /// <summary>
+    /// DTO для сущности контент
+    /// </summary>
+    public class PublicationDetailsDto
+    { 
+    }
+}
