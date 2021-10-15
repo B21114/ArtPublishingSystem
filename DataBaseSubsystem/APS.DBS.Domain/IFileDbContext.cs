b76d1729ca37381e03ea4a003e8277cb65d0aa5a
B21114 <b21114@yandex.ru>
2021-10-15 14:30:33 +0400

    #12 исправления2

--- DataBaseSubsystem/APS.DBS.Domain/IFileDbContext.cs
+++ DataBaseSubsystem/APS.DBS.Domain/IFileDbContext.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace APS.DBS.Domain
@@ -17,5 +18,12 @@ namespace APS.DBS.Domain
         /// Содержимое таблицы файлов.
         /// </summary>
         public DbSet<File> Files { get; set; }
+
+        /// <summary>
+        /// Асинхронно сохраняет все изменения, внесенные в этом контексте, в основную базу данных.
+        /// </summary>
+        /// <param name="cancellationToken">Объект для наблюдения за ожиданием завершения задачи.</param>
+        /// <returns></returns>
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
