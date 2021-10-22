2363cda10bdffedd6d14e86f72bfcaadfbed7ba6
B21114 <b21114@yandex.ru>
2021-10-22 14:13:08 +0400

    #17 temp

---
+++ ContentManagementSubsystem/APS.CMS.Application/Publications/Commands/DownloadPiblication/DownloadPublicationRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace APS.CMS.Application.Publications.Commands.DownloadPublication
+{
+    /// <summary>
+    /// Запрос загрузки файла.
+    /// </summary>
+    public class DownloadPublicationRequest : IRequest<DownloadPublicationResponse>
+    {
+
+        /// <summary>
+        /// Загружаемый файл.
+        /// </summary>
+        public IFormFile UploadFile { get; set; }
+
+        /// <summary>
+        /// Имя загружаемого файла.
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// Признак публичной доступности файла.
+        /// </summary>
+        public bool IsPublic { get; set; }
+
+
+    }
+}
---
+++ ContentManagementSubsystem/APS.CMS.Application/Publications/Commands/DownloadPiblication/DownloadPublicationResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APS.CMS.Application.Publications.Commands.DownloadPublication
+{
+    /// <summary>
+    /// Ответ.
+    /// </summary>
+    public class DownloadPublicationResponse
+    {
+        /// <summary>
+        ///  Хранит идентификатор новой записи.
+        /// </summary>
+        public Guid IdRecord { get; set; }
+    }
+}
